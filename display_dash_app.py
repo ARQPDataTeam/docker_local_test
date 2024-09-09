667f0b6ee630218a7b96875598e0d4bcb45a8fca
ARQPDataTeam <160161857+ARQPDataTeam@users.noreply.github.com>
2024-09-09 16:06:51 -0400

    Update display_dash_app.py

--- display_dash_app.py
+++ display_dash_app.py
@@ -101,5 +101,5 @@ def update_output(start_date, end_date):
 
 
 if __name__=='__main__':
-    app.run(debug=False, host='0.0.0.0', port=8050)
+    app.run(debug=False, host='0.0.0.0', port=8080)
     # app.run(debug=True)
